999b5267c5c9a6638741a5c903c505c1bb7fcdfa
nereadelbarrio <49400164+nereadelbarrio@users.noreply.github.com>
2022-08-24 17:37:35 +0200

    Update style.js

--- style.js
+++ style.js
@@ -1,18 +1,18 @@
 (function (blink) {
 	'use strict';
 
-	var castillopreescolar2022demoStyle = function () {
+	var preescolarCastillo2022demoStyle = function () {
 			blink.theme.styles.basic.apply(this, arguments);
 		},
 		page = blink.currentPage;
 
-	castillopreescolar2022demoStyle.prototype = {
+	preescolarCastillo2022demoStyle.prototype = {
 		//BK-15873 añadimos el estilo basic como parent para la herencia de los estilos del CKEditor
 		parent: blink.theme.styles.basic.prototype,
-		bodyClassName: 'content_type_clase_castillopreescolar2022demo',
+		bodyClassName: 'content_type_clase_preescolarCastillo2022demo',
 		extraPlugins: ['image2'],
 		ckEditorStyles: {
-			name: 'castillopreescolar2022demo',
+			name: 'preescolarCastillo2022demo',
 			styles: [
 
 				{ name: 'Título 01', element: 'h2', attributes: { 'class': 'bck-title1'} },
@@ -137,9 +137,9 @@
 		//BK15873 Quitamos la funcion getEditorStyles para que herede de parent
 	};
 
-	castillopreescolar2022demoStyle.prototype = _.extend({}, new blink.theme.styles.basic(), castillopreescolar2022demoStyle.prototype);
+	preescolarCastillo2022demoStyle.prototype = _.extend({}, new blink.theme.styles.basic(), preescolarCastillo2022demoStyle.prototype);
 
-	blink.theme.styles.castillopreescolar2022demo = castillopreescolar2022demoStyle;
+	blink.theme.styles.preescolarCastillo2022demo = preescolarCastillo2022demoStyle;
 
 })( blink );
 
@@ -160,8 +160,8 @@ $(document).ready(function () {
 		});
 
 	// BK-8433 cambiamos el logo de las slides por el del dominio
-	var src_logo = $('.content_type_clase_castillopreescolar2022demo').find('.logo_slide').attr('logo_dominio');
+	var src_logo = $('.content_type_clase_preescolarCastillo2022demo').find('.logo_slide').attr('logo_dominio');
 	if (typeof(src_logo) != 'undefined' && src_logo && src_logo != '' && src_logo.indexOf('gif1x1.gif') == -1) {
-		$('.content_type_clase_castillopreescolar2022demo').find('.logo-publisher').css('background-image', "url('"+src_logo+"')");
+		$('.content_type_clase_preescolarCastillo2022demo').find('.logo-publisher').css('background-image', "url('"+src_logo+"')");
 	}
 });
